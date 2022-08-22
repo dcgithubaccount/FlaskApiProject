997ddae20f3358b67f84a115649c7d918533c305
Sourcery AI <>
2022-08-22 19:47:54 +0000

    'Refactored by Sourcery'

--- tasks/test.py
+++ tasks/test.py
@@ -10,10 +10,10 @@ end_point = os.path.join(url, 'my_api', 'Contacts')
 @celery.task()
 def post_periodic_contacts():
     random_num = random.randint(1, 100)
-    username = 'user' + str(random_num)
-    first_name = username + 'Cho'
+    username = f'user{random_num}'
+    first_name = f'{username}Cho'
     last_name = 'Smith' if random_num%2 == 0 else 'Mrs Smith'
-    email = first_name+'.'+last_name+'@.example.com'
+    email = f'{first_name}.{last_name}@.example.com'
     inp = {
         'username': username,
         'first_name': first_name,
